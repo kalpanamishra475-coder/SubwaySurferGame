2c95932465712446c0253617e9e1e620e8f55a26
kalpanamishra475-coder <kalpanamishra475@gmail.com>
2026-03-29 10:25:15 +0530

    Add PlayerController script for movement and collision detection

---
+++ PlayerController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private bool isGrounded;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+        CheckJump();
+    }
+
+    private void MovePlayer()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal * moveSpeed, rb.velocity.y);
+        rb.velocity = movement;
+    }
+
+    private void CheckJump()
+    {
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
